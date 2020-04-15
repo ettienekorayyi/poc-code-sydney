b0eb1bf3e89082444d685be109996c024d0c7a8f
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-16 00:17:54 +1000

    DEV-008 style panel for panel

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import './navbar.css'
 import Logo from '../../assets/logo.png'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { MdClear } from 'react-icons/md'
+import { FaSearch, FaUserPlus } from 'react-icons/fa'
 
 const NavBar = () => {
   const [isHidden, setHidden] = useState(false)
@@ -25,8 +26,29 @@ const NavBar = () => {
           )}
         </div>
       </div>
+      {isHidden && (
+        <div className='dropdown-menu'>
+          <div className='dropdown-section'>
+            <div className='dropdown-icon'>
+              <FaSearch className='navigation-icon' />
+            </div>
+            <div className='dropdown-description'>Search</div>
+          </div>
+          <div className='dropdown-section'>
+            <div className='dropdown-icon'>
+              <FaUserPlus className='navigation-icon' />
+            </div>
+            <div className='dropdown-description'>Add New Mentor</div>
+          </div>
 
-      {isHidden && <div className='dropdown-menu'>PANEL HERE</div>}
+          <div className='dropdown-section loggedin-user'>
+            <div className='dropdown-icon'>
+              <FaUserPlus className='navigation-icon' />
+            </div>
+            <div className='dropdown-description'>Engrammar B</div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
