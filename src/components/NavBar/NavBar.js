77a3e3e9059c44e1d9885cc600768e0ce2a64500
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-15 23:33:17 +1000

    DEV-008 Added toggle effect to hide and show panel

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import Logo from '../../assets/logo.png'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdClear } from 'react-icons/md'
 
 const NavBar = () => {
+  const [isHidden, setHidden] = useState(false)
+
   return (
-    <div className='navbar'>
-      <div className='logo-section'>
-        <img src={Logo} className='logo' alt='code sydney logo' />
+    <div>
+      <div className='navbar'>
+        <div className='logo-section'>
+          <img src={Logo} className='logo' alt='code sydney logo' />
+        </div>
+        <div className='navigation'>
+          {!isHidden && (
+            <GiHamburgerMenu
+              className='icon'
+              onClick={(e) => setHidden(true)}
+            />
+          )}
+          {isHidden && (
+            <MdClear className='close-icon' onClick={(e) => setHidden(false)} />
+          )}
+        </div>
       </div>
-      <div className='navigation'>
-        <GiHamburgerMenu className='icon' />
-      </div>
+
+      {isHidden && <div className='dropdown-menu'>PANEL HERE</div>}
     </div>
   )
 }
