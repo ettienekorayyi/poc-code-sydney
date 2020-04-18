2e09e64fbbaa87e8db9610a574d8b5c2413201c9
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-18 20:48:35 +1000

    DEV-010 store map URL in a variable

--- src/components/Mentor/Mentor.js
+++ src/components/Mentor/Mentor.js
@@ -6,8 +6,7 @@ import { location } from '../../constants'
 
 const Mentor = () => {
   const mentorLocation = location[Math.floor(Math.random() * location.length)]
-
-  console.log(mentorLocation)
+  const mapURL = `https://maps.googleapis.com/maps/api/staticmap?center=${mentorLocation},AU&zoom=15&size=400x250&maptype=roadmap&markers=color:red%7Clabel:M%7C${mentorLocation},AU&key=AIzaSyBHiJzh1hfL8oF9mbYKc8p3lZieUVbPFck`
 
   return (
     <div className='mentor'>
@@ -25,14 +24,10 @@ const Mentor = () => {
             <div className='panel-location'>
               <MdLocationOn />
             </div>
-
             <div className='panel-location-text'>Location</div>
           </div>
           <div className='panel-content'>
-            <img
-              src={`https://maps.googleapis.com/maps/api/staticmap?center=${mentorLocation},AU&zoom=15&size=400x250&maptype=roadmap&markers=color:red%7Clabel:M%7C${mentorLocation},AU&key=AIzaSyBHiJzh1hfL8oF9mbYKc8p3lZieUVbPFck`}
-              className='map'
-            />
+            <img src={mapURL} className='map' />
           </div>
         </div>
       </div>
