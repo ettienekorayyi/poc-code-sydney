2f312fe9161c91cbc1c0f5aa23b4da84e1543f85
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-18 11:10:35 +1000

    DEV-010 Add map marker for mentor location

--- src/components/Mentor/Mentor.js
+++ src/components/Mentor/Mentor.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './mentor.css'
 import Engramar from '../../assets/engramar.png'
+import { MdLocationOn } from 'react-icons/md'
 
 const Mentor = () => {
   return (
@@ -9,6 +10,10 @@ const Mentor = () => {
         <img src={Engramar} className='mentor-photo' alt='Engramar' />
         <div className='mentor-name'>Engramar B</div>
         <div>Software Engineer</div>
+        <div>
+          <MdLocationOn className='location-marker' />
+          <span className='mentor-location'>Sydney, NSW</span>
+        </div>
       </div>
     </div>
   )
