d510787c2d869c1e830554092653a54f02382171
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-18 22:09:09 +1000

    DEV-017 Add hyperlink to the new mentor page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -39,12 +39,14 @@ const NavBar = () => {
               <div className='dropdown-description'>Search</div>
             </div>
           </a>
-          <div className='dropdown-section'>
-            <div className='dropdown-icon'>
-              <FaUserPlus className='navigation-icon' />
+          <a href='/create'>
+            <div className='dropdown-section'>
+              <div className='dropdown-icon'>
+                <FaUserPlus className='navigation-icon' />
+              </div>
+              <div className='dropdown-description'>Add New Mentor</div>
             </div>
-            <div className='dropdown-description'>Add New Mentor</div>
-          </div>
+          </a>
 
           <div className='dropdown-section loggedin-user'>
             <div className='dropdown-icon'>
