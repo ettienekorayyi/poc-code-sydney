4e7844ac91911744a47cd766b754893631680f99
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-18 18:52:22 +1000

    DEV-010 add location for mentor

--- src/components/Mentor/Mentor.js
+++ src/components/Mentor/Mentor.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './mentor.css'
 import Engramar from '../../assets/engramar.png'
+import { MdLocationOn } from 'react-icons/md'
 
 const Mentor = () => {
   return (
@@ -9,6 +10,10 @@ const Mentor = () => {
         <img src={Engramar} className='mentor-photo' alt='Engramar' />
         <div className='mentor-name'>Engramar B</div>
         <div>Software Engineer</div>
+        <div className='location'>
+          <MdLocationOn className='location-marker' />
+          Wolloomooloo, NSW
+        </div>
       </div>
     </div>
   )
