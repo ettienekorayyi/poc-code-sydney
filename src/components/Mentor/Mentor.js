8d448394fcf8c053b4260c3618ada29fd00af443
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-18 10:53:21 +1000

    DEV-010 Renders data for mentor photo name and title

--- src/components/Mentor/Mentor.js
+++ src/components/Mentor/Mentor.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './mentor.css'
+import Engramar from '../../assets/engramar.png'
 
 const Mentor = () => {
-  return <div>HELLO MENTOR</div>
+  return (
+    <div className='mentor'>
+      <div className='profile'>
+        <img src={Engramar} className='mentor-photo' alt='Engramar' />
+        <div className='mentor-name'>Engramar B</div>
+        <div>Software Engineer</div>
+      </div>
+    </div>
+  )
 }
 
 export default Mentor
