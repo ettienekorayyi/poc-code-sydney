1bc4a4d3405478cc2e12d61c799c1634f296e0b9
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-18 18:15:17 +1000

    DEV-010 renders users photo and title and description

--- src/components/Mentor/Mentor.js
+++ src/components/Mentor/Mentor.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './mentor.css'
 import Engramar from '../../assets/engramar.png'
-import { MdLocationOn } from 'react-icons/md'
 
 const Mentor = () => {
   return (
@@ -10,10 +9,6 @@ const Mentor = () => {
         <img src={Engramar} className='mentor-photo' alt='Engramar' />
         <div className='mentor-name'>Engramar B</div>
         <div>Software Engineer</div>
-        <div>
-          <MdLocationOn className='location-marker' />
-          <span className='mentor-location'>Sydney, NSW</span>
-        </div>
       </div>
     </div>
   )
