c3ac1610b97d2dc7a8571f04b24e4e95479c9e3d
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-21 20:47:08 +1000

    DEV-011 Create mentor form component as a modal

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './home.css'
 import Results from '../Results'
 import Search from '../Search'
+import MentorForm from '../MentorForm'
 
 const Home = () => {
   return (
     <div>
+      <MentorForm />
       <Search />
       <Results />
     </div>
