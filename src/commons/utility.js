18c8387f91503a346c66e9c0521714e51bba6efa
ettienekorayyi <corralstephenmelben@gmail.com>
2020-04-22 17:36:32 +1000

    Fixed the case sensitivity issue for the search functionality

--- src/commons/utility.js
+++ src/commons/utility.js
@@ -3,9 +3,9 @@ import { OPTION_TITLE, OPTION_NAME } from '../context/types'
 export let DropDownOptions = ({ dropDown, mentors, searchTerm }) => {
     switch (dropDown) {
         case OPTION_NAME:
-            return mentors.filter(m => m.fullName.includes(searchTerm));
+            return mentors.filter(m => m.fullName.toLowerCase().includes(searchTerm.toLowerCase()));
         case OPTION_TITLE:
-            return mentors.filter(m => m.title.includes(searchTerm));
+            return mentors.filter(m => m.title.toLowerCase().includes(searchTerm));
         default:
             return 'Please Select Name or Title';
     }
