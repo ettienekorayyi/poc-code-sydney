6198e1f749c3eae9b9f4099f3f778c92b699c3f7
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-22 19:05:08 +1000

    DEV-009 style changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './card.css'
 import PlaceHolderImage from '../../assets/placeholder_image.png'
 
-const Card = ({ id, fullName, title, photo }) => {
+const Card = ({ id, fullName, title, photo, description }) => {
   const photoURL = `https://codesydney-website.s3-ap-southeast-2.amazonaws.com/mentor/${id}.png`
 
   return (
@@ -14,6 +14,11 @@ const Card = ({ id, fullName, title, photo }) => {
       />
       <div className='card-fullName'>{fullName}</div>
       <div className='card-title'>{title}</div>
+      <div className='card-description'>
+        {description.length < 70
+          ? description
+          : `${description.slice(0, 70)}...`}
+      </div>
     </div>
   )
 }
