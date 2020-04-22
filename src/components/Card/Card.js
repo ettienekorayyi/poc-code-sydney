2625a98cb303208e2179cfb87dccc019a99f4bf8
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-22 17:55:02 +1000

    DEV-009 PR Comments regarding styling changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,21 @@
-import React from "react";
-import "./card.css";
-import PlaceHolderImage from "../../assets/placeholder_image.png";
+import React from 'react'
+import './card.css'
+import PlaceHolderImage from '../../assets/placeholder_image.png'
 
 const Card = ({ id, fullName, title, photo }) => {
-  const photoURL = `https://codesydney-website.s3-ap-southeast-2.amazonaws.com/mentor/${id}.png`;
+  const photoURL = `https://codesydney-website.s3-ap-southeast-2.amazonaws.com/mentor/${id}.png`
 
   return (
-    <div className="card">
-      <img className="card-image" src={photo ? photoURL : PlaceHolderImage} alt="user-profile" />
-      <div className="card-fullName">{fullName}</div>
-      <div className="card-title">{title}</div>
-      <button className="card-button">More Info</button>
+    <div className='card'>
+      <img
+        className='card-image'
+        src={photo ? photoURL : PlaceHolderImage}
+        alt='user-profile'
+      />
+      <div className='card-fullName'>{fullName}</div>
+      <div className='card-title'>{title}</div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
