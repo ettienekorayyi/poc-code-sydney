c0bb2d5fa20bb4fed76eb8370dbcb508f96c47b2
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-24 20:35:53 +1000

    Add logo to the desktop view page

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './desktop.css'
+import Logo from '../../assets/logo.png'
 
 const Desktop = () => {
   return (
-    <div>
+    <div className='desktop'>
+      <img src={Logo} alt='Code Sydney Logo' className='code-sydney-asset' />
       <h1>HERE IS THE DESKTOP VIEW</h1>
     </div>
   )
