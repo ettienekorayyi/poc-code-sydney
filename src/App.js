5e2689a57ed83b39319c4646d23c10a4f2659ba1
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-24 20:15:46 +1000

    Add displays controls to the desktop view

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import Mentor from './components/Mentor'
 const App = () => {
   return (
     <MentorState>
-      <NavBar />
-      <Router>
-        <Route exact path='/' component={Home} />
-        <Route path='/mentors' component={Home} />
-        <Route path='/mentor/:id' component={Mentor} />
-      </Router>
+      <div className='app'>
+        <NavBar />
+        <Router>
+          <Route exact path='/' component={Home} />
+          <Route path='/mentors' component={Home} />
+          <Route path='/mentor/:id' component={Mentor} />
+        </Router>
+      </div>
+      <div className='desktop-view'>
+        <h1>HERE IS THE DESKTOP VIEW. IT ONLY WORKS ON MOBILE</h1>
+      </div>
     </MentorState>
   )
 }
