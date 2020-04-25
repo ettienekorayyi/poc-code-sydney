74a162ea908452a5d36a79827af8bf27eb8e53ae
ettienekorayyi <corralstephenmelben@gmail.com>
2020-04-25 12:08:15 +1000

    Implemented Delete Mentor Functionality

--- src/context/types.js
+++ src/context/types.js
@@ -1,5 +1,7 @@
 export const GET_MENTOR = 'GET_MENTOR'
 export const GET_MENTORS = 'GET_MENTORS'
+export const DELETE_MENTOR = 'DELETE_MENTOR'
+export const DELETED_MENTOR_ERROR = 'DELETED_MENTOR_ERROR'
 export const MENTORS_ERROR = 'MENTORS_ERROR'
 export const POST_MENTOR = 'POST_MENTOR'
 
