8077b50c03370b26e47f95f71825004eea5baf1f
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-25 15:31:10 +1000

    DEV-021 add DeleteConfirmation component

---
+++ src/components/DeleteConfirmation/DeleteConfirmation.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './deleteconfirmation.css'
+
+const DeleteConfirmation = () => {
+  return <div>DELETE CONFIRMATION COMPONENT</div>
+}
+
+export default DeleteConfirmation
