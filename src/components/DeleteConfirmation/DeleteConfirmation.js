e241c8fb70e7a5542a0548a2a18095b21cf93725
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-25 18:46:57 +1000

    DEV-021 Create the delete confirmation component

--- src/components/DeleteConfirmation/DeleteConfirmation.js
+++ src/components/DeleteConfirmation/DeleteConfirmation.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './deleteconfirmation.css'
 
 const DeleteConfirmation = () => {
-  return <div>DELETE CONFIRMATION COMPONENT</div>
+  const [modalOpened, setModalOpened] = useState(false)
+
+  const modalToggle = () => {
+    setModalOpened(!modalOpened)
+  }
+
+  const coverClass = modalOpened
+    ? 'delete-modal-cover delete-modal-cover-active'
+    : 'delete-modal-cover'
+  const containerClass = modalOpened
+    ? 'delete-modal-container delete-modal-container-active'
+    : 'delete-modal-container'
+
+  return (
+    <div>
+      <button className='delete-button delete-background' onClick={modalToggle}>
+        Delete
+      </button>
+
+      <div className={containerClass}>
+        ARE YOU FUCKING SURE THAT YOU WANT TO DELETE THIS FUCKING USER?
+      </div>
+
+      <div className={coverClass} onClick={modalToggle}></div>
+    </div>
+  )
 }
 
 export default DeleteConfirmation
